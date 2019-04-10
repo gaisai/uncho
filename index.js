8b3310d3c72fd5f7bcbfb9d3025226c2a6c56b69
tomoki_saito <saito_tomoki@tomokisaito-macbookpro.local>
2019-04-10 21:59:45 +0900

    change obj size

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
 function aaa(){
 
-	t=r+r*Math.sin(i*5/360)
-	l=r+r*Math.cos(i*5/360)
+	rh=window.innerHeight/4
+	rw=window.innerWidth/4
+	t=rh+rh*Math.sin(i*5/360)
+	l=rw+rw*Math.cos(i*5/360)
+
 	a.style.top=t
 	a.style.left=l
 
 	i++;
 	olog=log
-	log="("+w+","+h+")"
+	log="("+r+","+window.innerWidth+","+window.innerHeight+","+a.style.width+","+a.style.height+")"
 	if(log!=olog){
 		console.log("(w,h)="+log);
 	}
@@ -19,13 +22,16 @@ function aaa(){
 i=0
 var a=document.getElementById("ma-n");
 a.style.position="relative"
-a.style.width=100
-a.style.height=100
-var w,h,t,l,log,olog
+a.style.backgroundColor="red"
+//a.style.display=block
+a.style.width="50%"
+a.style.height="50%"
+
+var w,h,t,l,log,olog,rw,rh
 var r=100
 
 var a=document.getElementById("ma-n");
-a.innerHTML="ww<br>ww";
+a.innerHTML="<center>ww<br>ww</center>";
 console.log("ha?");
 
 aaa();
