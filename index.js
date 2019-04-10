4ae87d9baaf2ff2d426710547c2eb99a4c5831f3
tomoki_saito <saito_tomoki@tomokisaito-macbookpro.local>
2019-04-10 21:16:47 +0900

    moving

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 
+i=0
 var a=document.getElementById("ma-n");
-a.innerHTML="omanko???";
+a.style.position="relative"
+a.style.width=100
+a.style.height=100
+var w,h,t,l,log,olog
+var r=100
+
+function aaa(){
+
+	t=r+r*Math.sin(i*5/360)
+	l=r+r*Math.cos(i*5/360)
+	a.style.top=t
+	a.style.left=l
+
+	i++;
+	olog=log
+	log="("+w+","+h+")"
+	if(log!=olog){
+		console.log("(w,h)="+log);
+	}
+
+	setTimeout(aaa,3);
+}
+
+
+var a=document.getElementById("ma-n");
+a.innerHTML="ww<br>ww";
 console.log("ha?");
+
+aaa();
