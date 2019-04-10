428261a25e62eb8d94b8ed8e8a16953622b052d0
tomoki_saito <saito_tomoki@tomokisaito-macbookpro.local>
2019-04-10 22:34:15 +0900

    chenge that the moving is round

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function aaa(){
 
 	rh=(window.innerHeight-h)/2
 	rw=(window.innerWidth-w)/2
-	t=rh+rh*Math.sin((3*i/fps)*2*Math.PI)
-	l=rw+rw*Math.cos((3*i/fps)*2*Math.PI)
+	t=rh+rh*Math.sin((5*i/fps)*2*Math.PI)
+	l=rw+rw*Math.cos((5*i/fps)*2*Math.PI)
 
 	a.style.top=t
 	a.style.left=l
@@ -28,15 +28,25 @@ function aaa(){
 fps=300
 var i=0,j=0
 
+var c=document.getElementById("a");
+//c.innerHTML=
+
 var a=document.getElementById("ma-n");
 var b=document.getElementById("ha");
 a.style.position="relative"
+b.style.position="relative"
+b.style.top=-85
+b.style.left=0
+b.style.width=300
+b.style.height=300
+b.style.backgroundColor="black"
 a.style.backgroundColor="red"
 //a.style.display=block
 w=30
-h=30
+h=50
 a.style.width=w
 a.style.height=h
+a.style.borderRadius="50%"
 
 var w,h,t,l,log,olog,rw,rh
 var r=100
